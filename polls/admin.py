f29f39a6231d4f7cfe4d2efd83dcf15ee64bbdb6
HY\harry <hy546880109@qq.com>
2022-09-07 17:33:39 +0800

    django入门完成

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuetsionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [(None, {'fields':['question_text']}),
+                 ('date',{'fields':['pub_date'], 'classes':['collapse']}),
+                 ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+admin.site.register(Question,QuetsionAdmin)
+
+
